87ade756e7c1a520062fd9e540eaf0f3e8c53fe3
Andrew Ealovega <avealov@umich.edu>
2022-07-29 19:09:56 -0400

    Add CI (#3)
    * Add linters
    
    Signed-off-by: Andrew Ealovega <Andrew@Ealovega.dev>
    
    * Apply linters
    
    Signed-off-by: Andrew Ealovega <Andrew@Ealovega.dev>
    
    * I am not immune to this irony
    
    Signed-off-by: Andrew Ealovega <Andrew@Ealovega.dev>

---
+++ .github/node_template.md
@@ -0,0 +1,29 @@
+# <NodeNameHere>
+
+## Summary
+
+Summary of functionality
+
+## Topics 
+
+### Publishes
+
+- exampleTopic: description of topic published
+
+### Subscribes
+
+- exampleTopicSub: description of topic subbed
+
+## Params 
+
+- exampleParam: int that does a thing
+
+## Potential Improvements
+
+This node trash
+
+# Launch 
+
+'./path/to/launchfile'
+
+other launchfile stuff people should know, like a usage section
--- src/twist_to_ackermann.cpp
+++ src/twist_to_ackermann.cpp
@@ -1,18 +1,20 @@
-#include "twist_to_ackermann/tta.hpp"
 #include <cmath>
 #include <functional>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 
-namespace tta {
+#include "twist_to_ackermann/tta.hpp"
 
-/// Finds the appropriate steering angle for some turning radius, which is defined by the 
+namespace tta
+{
+/// Finds the appropriate steering angle for some turning radius, which is defined by the
 /// transient velocity of the vehicle along that curve (just the current velocity in this instance),
 /// and the rate that curve changes (angular z rotation). This angle is effected by vehicle wheelbase.
 ///
 /// All units are in m/s or r/s.
-double convert_trans_rot_vel_to_steering_angle(double vel, double omega, double wheelbase) {
-  if(omega == 0 || vel == 0) {
+double convert_trans_rot_vel_to_steering_angle(double vel, double omega, double wheelbase)
+{
+  if (omega == 0 || vel == 0) {
     return 0;
   }
 
@@ -24,51 +26,49 @@ double convert_trans_rot_vel_to_steering_angle(double vel, double omega, double 
 }
 
 TwistToAckermann::TwistToAckermann(rclcpp::NodeOptions options)
-    : Node("twist_to_ackermann", options) {
+: Node("twist_to_ackermann", options)
+{
+  _use_stamps = this->declare_parameter("use_stamps", false);
 
-      _use_stamps = this->declare_parameter("use_stamps", false);
+  if (_use_stamps) {
+    _twists_sub = this->create_subscription<geometry_msgs::msg::TwistStamped>(
+      "/nav_vel", 10, std::bind(&TwistToAckermann::twists_cb, this, std::placeholders::_1));
 
-      if(_use_stamps) {
-        _twists_sub = this->create_subscription<geometry_msgs::msg::TwistStamped>(
-          "/nav_vel", 
-          10, 
-          std::bind(&TwistToAckermann::twists_cb, this, std::placeholders::_1)
-          );
-        
-        _acks_pub = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("/ack_vel", 10);
-      } else {
-        _twist_sub = this->create_subscription<geometry_msgs::msg::Twist>(
-          "/nav_vel", 
-          10,
-          std::bind(&TwistToAckermann::twist_cb, this, std::placeholders::_1)
-          );
-        
-        _ack_pub = this->create_publisher<ackermann_msgs::msg::AckermannDrive>("/ack_vel", 10);
-      } 
+    _acks_pub = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("/ack_vel", 10);
+  } else {
+    _twist_sub = this->create_subscription<geometry_msgs::msg::Twist>(
+      "/nav_vel", 10, std::bind(&TwistToAckermann::twist_cb, this, std::placeholders::_1));
 
-      _wheelbase = this->declare_parameter("wheelbase", 1.0);
+    _ack_pub = this->create_publisher<ackermann_msgs::msg::AckermannDrive>("/ack_vel", 10);
+  }
+
+  _wheelbase = this->declare_parameter("wheelbase", 1.0);
 }
 
-void TwistToAckermann::twist_cb(geometry_msgs::msg::Twist::SharedPtr msg) {
+void TwistToAckermann::twist_cb(geometry_msgs::msg::Twist::SharedPtr msg)
+{
   ackermann_msgs::msg::AckermannDrive out{};
   out.speed = msg->linear.x;
-  out.steering_angle = convert_trans_rot_vel_to_steering_angle(msg->linear.x, msg->angular.z, this->_wheelbase);
+  out.steering_angle =
+    convert_trans_rot_vel_to_steering_angle(msg->linear.x, msg->angular.z, this->_wheelbase);
 
   this->_ack_pub->get()->publish(out);
 }
 
-void TwistToAckermann::twists_cb(geometry_msgs::msg::TwistStamped::SharedPtr msg) {
+void TwistToAckermann::twists_cb(geometry_msgs::msg::TwistStamped::SharedPtr msg)
+{
   ackermann_msgs::msg::AckermannDriveStamped out{};
   out.drive.speed = msg->twist.linear.x;
-  out.drive.steering_angle = 
-    convert_trans_rot_vel_to_steering_angle(msg->twist.linear.x, msg->twist.angular.z, this->_wheelbase);
+  out.drive.steering_angle = convert_trans_rot_vel_to_steering_angle(
+    msg->twist.linear.x, msg->twist.angular.z, this->_wheelbase);
 
   this->_acks_pub->get()->publish(out);
 }
 
-} // namespace tta
+}  // namespace tta
 
-int main(int argc, char **argv) {
+int main(int argc, char ** argv)
+{
   rclcpp::init(argc, argv);
   rclcpp::executors::SingleThreadedExecutor exec;
   rclcpp::NodeOptions options;
